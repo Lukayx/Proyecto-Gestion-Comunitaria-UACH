52fc0c45acd6c851c3f5b603911c5b42ee735e39
Lukayx <olavarriafernando36@gmail.com>
2023-12-10 20:53:05 -0300

    XD

--- Backend/src/Controllers/organizationController.ts
+++ Backend/src/Controllers/organizationController.ts
@@ -7,45 +7,24 @@ import { Organization } from '../Interfaces/Organization';
 export async function uploadOrganizations(req: Request, res: Response) {
   try {
     const organizaciones: Organization[] = req.body;
-    // Intenta conectar a la base de datos
-    let conn: Connection;
+    let conn: Connection; // Intenta conectar a la base de datos
     try {
       conn = await connect();
     } catch (error) {
-      res.status(500).json({ mensaje: 'Error en el servidor' });
+      res.status(500).json({ mensaje: 'Error al intentar conectar con la base de datos.' });
       return; // Detiene la ejecución si no se pudo conectar a la base de datos
     }
-    // Procesa cada organización
-    for (let i = 0; i < organizaciones.length; i++) {
+    for (let i = 0; i < organizaciones.length; i++) { // Procesa cada organización
       let elemento: Organizations = new Organizations(organizaciones);
-      // Verifica si es posible agregar la organización
-      if (elemento.isAddable(i)) {
+      if (elemento.isAddable(i)) { // Verifica si es posible agregar la organización
         try {
           // Verifica si ya existe una organización con el mismo numOrg
-          const [rows] = await conn.execute(
-            'SELECT 1 FROM organizaciones WHERE numOrg = ? LIMIT 1',
-            [organizaciones[i].numOrg]
-          );
-          // Verificar si se encontraron filas
-          if (Array.isArray(rows) && rows.length === 0) {
-            // Si no existe, entonces se puede agregar
+          const [rows] = await conn.execute('SELECT 1 FROM organizaciones WHERE numOrg = ? LIMIT 1',[organizaciones[i].numOrg]); 
+          if (Array.isArray(rows) && rows.length === 0) {// Verificar que es un arreglo y que su largo es 0 (NO HAY NADIE CON EL MISMO numOrg)
+            const values = Object.values(organizaciones[i]);
             await conn.execute(
-              `INSERT INTO organizaciones (numOrg, nombreOrg, rut, origen, comuna, region, direccion, tipo, fechaConceso, fechaRecepcion, clasificacion, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-              [
-                organizaciones[i].numOrg,
-                organizaciones[i].nombreOrg,
-                organizaciones[i].rut,
-                organizaciones[i].origen,
-                organizaciones[i].comuna,
-                organizaciones[i].region,
-                organizaciones[i].direccion,
-                organizaciones[i].tipo,
-                organizaciones[i].fechaConceso,
-                organizaciones[i].fechaRecepcion,
-                organizaciones[i].clasificacion,
-                organizaciones[i].estado,
-              ]
-            );
+              `INSERT INTO organizaciones (numOrg, nombreOrg, rut, origen, comuna, region, direccion, tipo, fechaConceso, fechaRecepcion, clasificacion, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
+              [values] );
           } else {
             console.log('El numOrg:', elemento.organizaciones[i].numOrg, 'ya existe en la tabla.');
           }
@@ -80,12 +59,38 @@ export async function getNameOrganizations(req: Request, res: Response) {
 }
 
 export async function uploadOrganization(req: Request, res:Response) {
-  let conn: Connection;
   try {
-    conn = await connect();
+    let conn: Connection;
+    try {
+      conn = await connect();
+    } catch (error) {
+      res.status(500).json('Error al intentar conectarse con la base de datos');
+      return;
+    }
+    const organizaciones: Organization[] = req.body;
+    let elemento: Organizations = new Organizations(organizaciones);
+    if (elemento.isAddable(0)) { // Verifica si es posible agregar la organización
+      try {
+        // Verifica si ya existe una organización con el mismo numOrg
+        const [rows] = await conn.execute('SELECT 1 FROM organizaciones WHERE numOrg = ? LIMIT 1',[organizaciones[0].numOrg]); 
+        if (Array.isArray(rows) && rows.length === 0) {// Verificar que es un arreglo y que su largo es 0 (NO HAY NADIE CON EL MISMO numOrg)
+          const values = Object.values(organizaciones[0]);
+          await conn.execute(
+            `INSERT INTO organizaciones (numOrg, nombreOrg, rut, origen, comuna, region, direccion, tipo, fechaConceso, fechaRecepcion, clasificacion, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
+            [values] );
+        } else {
+          console.log('El numOrg:', elemento.organizaciones[0].numOrg, 'ya existe en la tabla.');
+        }
+      } catch (error) {
+        console.error('Error al intentar agregar la organización:', error);
+        res.status(500).json({ mensaje: 'Error en el servidor' });
+      }
+    } else {
+      console.log('Hay un elemento null en la fila');
+    }
+    res.status(200).json({ mensaje: 'Solicitud POST recibida correctamente' });
   } catch (error) {
-    res.status(500).json('Error al intentar conectarse con la base de datos');
-    return;
+    console.error('Error al procesar la solicitud:', error);
+    res.status(500).json({ mensaje: 'Error en el servidor' });
   }
-
 }
