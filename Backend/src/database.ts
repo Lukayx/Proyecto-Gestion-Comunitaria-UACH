b2d4f6b2770b3b2e7c254e455bd9c952fc81e6c2
Lukayx <olavarriafernando36@gmail.com>
2023-12-13 23:50:41 -0300

    createPool agregado (cambiado)

--- Backend/src/database.ts
+++ Backend/src/database.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export async function connect() {
-  const connection = await mysql.createConnection({
+  const pool = await mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
     connectionLimit: 10,
   });
-  return connection;
+  return pool;
 }
--- Frontend/src/components/Novedades.tsx
+++ Frontend/src/components/Novedades.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 
 const Novedades: FunctionComponent<{ title: string; content: string }> = ({ title, content }) => {
   return (
